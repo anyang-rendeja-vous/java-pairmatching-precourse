15e515435857d1604d26d9523c0433aeed53c06b
jmj005 <jmj00005@naver.com>
2022-12-09 13:22:20 +0900

    feat(PairMatchingController): crew 목록 랜덤 섞기 기능 구현

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,16 +1,20 @@
 package pairmatching.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class PairMatchingController {
 
     public void run() throws IOException {
         List<String> backendCrew = getCrew("src/main/resources/backend-crew.md");
         List<String> frontendCrew = getCrew("src/main/resources/frontend-crew.md");
+        List<String> shuffledBackendCrew = getShuffledCrew(backendCrew);
+        List<String> shuffledFrontendCrew = getShuffledCrew(frontendCrew);
     }
 
     public List<String> getCrew(String fileName) throws IOException {
@@ -25,5 +29,10 @@ public class PairMatchingController {
         }
         bufferedReader.close();
         return crew;
+    }
+
+    public List<String> getShuffledCrew(List<String> crew) {
+        List<String> shuffledCrew = Randoms.shuffle(crew);
+        return shuffledCrew;
     }
 }
