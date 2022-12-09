de1508fe71ef67e72d2232b2425950eaf11234b4
jmj005 <jmj00005@naver.com>
2022-12-09 13:27:19 +0900

    refactor(Crew): domain Crew 분리

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -1,38 +1,18 @@
 package pairmatching.controller;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import pairmatching.domain.Crew;
 
 public class PairMatchingController {
 
+    Crew crew = new Crew();
+
     public void run() throws IOException {
-        List<String> backendCrew = getCrew("src/main/resources/backend-crew.md");
-        List<String> frontendCrew = getCrew("src/main/resources/frontend-crew.md");
-        List<String> shuffledBackendCrew = getShuffledCrew(backendCrew);
-        List<String> shuffledFrontendCrew = getShuffledCrew(frontendCrew);
+        List<String> backendCrew = crew.getCrew("src/main/resources/backend-crew.md");
+        List<String> frontendCrew = crew.getCrew("src/main/resources/frontend-crew.md");
+        List<String> shuffledBackendCrew = crew.getShuffledCrew(backendCrew);
+        List<String> shuffledFrontendCrew = crew.getShuffledCrew(frontendCrew);
     }
 
-    public List<String> getCrew(String fileName) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        List<String> crew = new ArrayList<String>();
-        while (true) {
-            String line = bufferedReader.readLine();
-            if (line == null) {
-                break;
-            }
-            crew.add(line);
-        }
-        bufferedReader.close();
-        return crew;
-    }
-
-    public List<String> getShuffledCrew(List<String> crew) {
-        List<String> shuffledCrew = Randoms.shuffle(crew);
-        return shuffledCrew;
-    }
 }
