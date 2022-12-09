bc550b0bd8117c8c6be803ff10cda7360054d243
minju412 <ln8847@ajou.ac.kr>
2022-12-09 17:21:30 +0900

    feat(Controller): add iteration
    - 결과 출력 후 다음 입력을 기다리도록

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -8,6 +8,7 @@ import pairmatching.domain.Level;
 import pairmatching.domain.MatchingResults;
 import pairmatching.domain.Mission;
 import pairmatching.domain.Pair;
+import pairmatching.domain.menu.Menu;
 import pairmatching.util.CrewReader;
 import pairmatching.view.InputView;
 import pairmatching.view.OutputView;
@@ -27,19 +28,40 @@ public class PairMatchingController {
     public void run() {
         initialCrews();
 
-        outputView.printMenu();
-        String answer = inputView.readAnswer();
-        String[] split = answer.split(", "); // 과정 레벨 미션
+        while (true) {
+            outputView.printInitialMenu();
+            String mainChoice = inputView.readMainMenu();
 
-        Course course = Course.findCourse(split[0]);
-        Level level = Level.findLevel(split[1]); // TODO: 레벨이랑 미션이 올바른지 검증
-        Mission mission = Mission.findMissionByName(split[2]);
+            if (Menu.PAIR_MATCHING.isSamePrefix(mainChoice)) {
+                outputView.printMenu();
+                String answer = inputView.readAnswer();
+                String[] split = answer.split(", "); // 과정 레벨 미션
 
-        // 1. 페어 매칭
-        MatchingResults matchingResults = new MatchingResults(mission, CrewRepository.getCrewsByCourse(course));
-        List<Pair> resultsByMission = matchingResults.getResultsByMission(mission);
-        outputView.printResult(resultsByMission);
+                Course course = Course.findCourse(split[0]);
+                Level level = Level.findLevel(split[1]); // TODO: 레벨이랑 미션이 올바른지 검증
+                Mission mission = Mission.findMissionByName(split[2]);
 
+                boolean wantReMatch = true;
+                if (MatchingResults.isAlreadyExist(mission)) {
+                    String reMatchingAnswer = inputView.readReMatching();
+                    if (reMatchingAnswer.equals("아니오")) {
+                        wantReMatch = false;
+                    }
+                }
+
+                if (wantReMatch) {
+                    // 1. 페어 매칭
+                    MatchingResults matchingResults = new MatchingResults(mission,
+                            CrewRepository.getCrewsByCourse(course));
+                    List<Pair> resultsByMission = matchingResults.getResultsByMission(mission);
+                    outputView.printResult(resultsByMission);
+                }
+            }
+
+            if (Menu.QUIT.isSamePrefix(mainChoice)) {
+                break;
+            }
+        }
     }
 
     private void initialCrews() {
