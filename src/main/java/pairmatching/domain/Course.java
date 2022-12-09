b92fee19979af9898e3ef1decba6510c8540fef0
minju412 <ln8847@ajou.ac.kr>
2022-12-09 13:32:57 +0900

    feat(Course): add methods to print courses
    - 정해진 형식대로 코스를 출력하기 위해 메서드 추가

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,9 +1,13 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
+    private static final String DELIMITER = " | ";
     private String name;
 
     Course(String name) {
@@ -11,4 +15,13 @@ public enum Course {
     }
 
     // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public static String reformat() {
+        return Arrays.stream(Course.values())
+                .map(Course::getName)
+                .collect(Collectors.joining(DELIMITER));
+    }
 }
