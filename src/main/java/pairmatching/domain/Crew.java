b5f2848c0e574715749eb773f9be23015a7a5f0d
jmj005 <jmj00005@naver.com>
2022-12-09 14:40:06 +0900

    feat(Crew): Crew enum 생성

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,30 +1,7 @@
 package pairmatching.domain;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Crew {
 
-    public List<String> getCrew(String fileName) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        List<String> crew = new ArrayList<>();
-        while (true) {
-            String line = bufferedReader.readLine();
-            if (line == null) {
-                break;
-            }
-            crew.add(line);
-        }
-        bufferedReader.close();
-        return crew;
-    }
-
-    public List<String> getShuffledCrew(List<String> crew) {
-        List<String> shuffledCrew = Randoms.shuffle(crew);
-        return shuffledCrew;
-    }
+    private Course course;
+    private String name;
 }
