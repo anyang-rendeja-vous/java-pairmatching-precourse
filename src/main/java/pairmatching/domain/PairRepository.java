922ddc51a8bf46f359b322a65cf1e0522f316e2d
jmj005 <jmj00005@naver.com>
2022-12-11 04:44:40 +0900

    feat(PairRepository): 페어 리스트 저장 기능 구현

--- src/main/java/pairmatching/domain/PairRepository.java
+++ src/main/java/pairmatching/domain/PairRepository.java
@@ -1,2 +1,33 @@
-package pairmatching.domain;public class PairRepository {
+package pairmatching.domain;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class PairRepository {
+
+    private final LinkedHashMap<String, List<Pair>> backendPairs = new LinkedHashMap<String, List<Pair>>();
+    private final LinkedHashMap<String, List<Pair>> frontendPairs = new LinkedHashMap<String, List<Pair>>();
+
+    public void resetPairRepository() {
+        backendPairs.clear();
+        frontendPairs.clear();
+    }
+
+    //페어 리스트 추가
+    public void addPair(List<String> format, List<Pair> pairs) {
+        if (format.get(0).equals(Course.BACKEND.getCourseName())) {
+            addBackendPair(format.get(2), pairs);
+        }
+        addFrontendPair(format.get(2), pairs);
+    }
+
+    //백엔드 페어 리스트 추가
+    private void addBackendPair(String mission, List<Pair> pairs) {
+        backendPairs.put(mission, pairs);
+    }
+
+    //프론트엔드 페어 리스트 추가
+    private void addFrontendPair(String mission, List<Pair> pairs) {
+        frontendPairs.put(mission, pairs);
+    }
 }
