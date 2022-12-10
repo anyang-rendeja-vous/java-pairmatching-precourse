b940ecbf8484c0599eaa0b4936b478331282cbe3
jmj005 <jmj00005@naver.com>
2022-12-11 03:40:13 +0900

    feat(CrewRepository): 크루 페어 매칭 기능 구현

--- src/main/java/pairmatching/domain/CrewRepository.java
+++ src/main/java/pairmatching/domain/CrewRepository.java
@@ -1,61 +1,51 @@
 package pairmatching.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import pairmatching.controller.FileController;
 
-public class CrewList {
+public class CrewRepository {
 
-    private final String BACKEND_MD = "src/main/resources/backend-crew.md";
-    private final String FRONTEND_MD = "src/main/resources/frontend-crew.md";
+    FileController fileController = new FileController();
 
-    //랜덤 페어 반환
-    public List<List<String>> getPair(String course) {
+    public List<Crew> getShuffledCrews(String course) {
+        List<Crew> crews = getCrews(course);
+        return Randoms.shuffle(crews);
+    }
+
+    public List<Crew> getCrews(String course) {
         if (course.equals(Course.BACKEND.getCourseName())) {
-            try {
-                return getShuffledCrew(getCrew(BACKEND_MD));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            return fileController.readBackendCrews();
         }
-        try {
-            return getShuffledCrew(getCrew(FRONTEND_MD));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return fileController.readFrontendCrews();
     }
 
-    //파일 읽어오기
-    public List<String> getCrew(String fileName) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        List<String> crew = new ArrayList<>();
-        while (true) {
-            String line = bufferedReader.readLine();
-            if (line == null) {
-                break;
+    public List<Pair> getPairs(List<Crew> shuffledCrew) {
+        List<Pair> pairs = new ArrayList<Pair>();
+        while (shuffledCrew.size() > 0){
+            if (shuffledCrew.size() == 3) {
+                pairs.add(new Pair(getOddParis(shuffledCrew)));
             }
-            crew.add(line);
+            if (shuffledCrew.size() > 3) {
+                pairs.add(new Pair(getEvenPairs(shuffledCrew)));
+            }
         }
-        bufferedReader.close();
-        return crew;
+        return pairs;
     }
 
-    //랜덤
-    public List<List<String>> getShuffledCrew(List<String> crew) {
-        List<String> shuffledCrew = Randoms.shuffle(crew);
-        List<List<String>> splitShuffledCrew = new ArrayList<>();
-        for (int i = 0; i < shuffledCrew.size() - 1; i += 2) {
-            splitShuffledCrew.add(Arrays.asList(shuffledCrew.get(i), shuffledCrew.get(i + 1)));
-        }
-        if (shuffledCrew.size() % 2 != 0) {
-            List<String> tmp = new ArrayList<String>(splitShuffledCrew.get(splitShuffledCrew.size() - 1));
-            tmp.add(shuffledCrew.get(shuffledCrew.size() - 1));
-            splitShuffledCrew.set(splitShuffledCrew.indexOf(splitShuffledCrew.get(splitShuffledCrew.size() - 1)), tmp);
-        }
-        return splitShuffledCrew;
+    public List<Crew> getOddParis(List<Crew> crew) {
+        List<Crew> oddPairs = new ArrayList<Crew>();
+        oddPairs.add(crew.remove(0));
+        oddPairs.add(crew.remove(0));
+        oddPairs.add(crew.remove(0));
+        return oddPairs;
+    }
+
+    public List<Crew> getEvenPairs(List<Crew> crew) {
+        List<Crew> evenPairs = new ArrayList<Crew>();
+        evenPairs.add(crew.remove(0));
+        evenPairs.add(crew.remove(0));
+        return evenPairs;
     }
 }
