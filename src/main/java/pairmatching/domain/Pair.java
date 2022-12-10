0355316d5760112b0ae8147e9706b2bcc77bb1fd
jmj005 <jmj00005@naver.com>
2022-12-11 02:59:19 +0900

    refactor(Pair): Pair refactoring

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -1,2 +1,27 @@
-package pairmatching.domain;public class Pair {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Pair {
+
+    private final List<Crew> pair;
+
+    public Pair(List<Crew> pairs) {
+        this.pair = pairs;
+    }
+
+    public List<Crew> getPairs() {
+        return Collections.unmodifiableList(pair);
+    }
+
+    @Override
+    public String toString() {
+        List<String> crews = new ArrayList<String>();
+        for (Crew crew : pair) {
+            crews.add(crew.toString());
+        }
+        return String.join(" : ", crews) + System.lineSeparator();
+    }
 }
