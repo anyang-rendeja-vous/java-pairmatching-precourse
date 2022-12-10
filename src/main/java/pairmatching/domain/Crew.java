374cac04a9bb199d77bb8e2c5884814b471b58e4
jmj005 <jmj00005@naver.com>
2022-12-11 04:24:11 +0900

    refactor(Crew): Crew refactoring

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -3,13 +3,23 @@ package pairmatching.domain;
 public class Crew {
 
     private Course course;
-    private String name;
+    private String crew;
 
-    Crew(String name) {
-        this.name = name;
+    public Crew(Course course, String crew) {
+        this.course = course;
+        this.crew = crew;
     }
 
-    public String getCrewName() {
-        return name;
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getCrew() {
+        return crew;
+    }
+
+    @Override
+    public String toString() {
+        return crew;
     }
 }
