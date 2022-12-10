428a51df409e2dd3c2df4ce7cea9cec753f5c875
jmj005 <jmj00005@naver.com>
2022-12-11 03:00:28 +0900

    feat(Pairs): pair의 목록를 저장할 class 생성

--- src/main/java/pairmatching/domain/Pairs.java
+++ src/main/java/pairmatching/domain/Pairs.java
@@ -1,2 +1,26 @@
-package pairmatching.domain;public class Pairs {
+package pairmatching.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pairs {
+
+    private List<Pair> pairs;
+
+    public Pairs(List<Pair> pairs) {
+        this.pairs = pairs;
+    }
+
+    public List<Pair> getPairs() {
+        return pairs;
+    }
+
+    @Override
+    public String toString() {
+        List<String> pairList = new ArrayList<>();
+        for (Pair pair : pairs) {
+            pairList.add(pair.toString());
+        }
+        return pairList.toString();
+    }
 }
